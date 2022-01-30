8776bb9af9e0f868a4d58b974f19df368ea1e004
draganov <draganov89@mail.bg>
2022-01-31 01:54:50 +0200

    RequestCard added

---
+++ Suchmasy/Suchmasy/ViewComponents/RequestCard.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Suchmasy.Models;
+
+namespace Suchmasy.ViewComponents
+{
+    public class RequestCard : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(Request requestCard)
+        {
+            return View(requestCard);
+        }
+    }
+}
